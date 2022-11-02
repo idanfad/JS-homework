ee0581597f8e6b06c3d70fae769d8fa3847e3c49
idanfad <114670264+idanfad@users.noreply.github.com>
2022-11-02 19:19:20 +0200

    Add files via upload

---
+++ jshomework/script.js
@@ -0,0 +1,28 @@
+let overallPrice = 0;
+
+function tomatoFunc() {
+    
+    let pizza = prompt("האם אתה רוצה תוספת גבינה?");
+    if (pizza == "כן") {
+        alert('הזמנתך: פיצה עם תוספת גבינה');
+    } else {
+        alert('הזמנתך: פיצה ללא תוספת גבינה');
+    }
+    
+    console.log(pizza);
+}
+
+function func2() {
+    
+    let pizza = prompt("האם אתה רוצה תוספת קציצה?");
+    if (pizza == "כן") {
+        alert('הזמנתך: המבורגר עם תוספת קציצה');
+    } else {
+        alert('הזמנתך: המבורגר ללא תוספת קציצה');
+    }
+    
+    console.log(pizza);
+}
+
+
+
